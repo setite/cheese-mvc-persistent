188ae152b064d97dc5cc571b04899d15ef41c398
setite <setyte@gmail.com>
2018-12-19 20:18:56 -0600

    had to manually add forms and it didnt commit

---
+++ src/main/java/org/launchcode/models/forms/AddMenuItemForm.java
@@ -0,0 +1,55 @@
+package org.launchcode.models.forms;
+
+import org.launchcode.models.Cheese;
+import org.launchcode.models.Menu;
+
+/**
+ * Created by Sir Monkey Bottoms
+ */
+
+public class AddMenuItemForm {
+
+    private Menu menu;
+    private Iterable<Cheese> cheeses;
+    private int menuId;
+    private int cheeseId;
+
+    public AddMenuItemForm() {}
+
+    public AddMenuItemForm(Iterable<Cheese> cheeses, Menu menu) {
+        this.cheeses = cheeses;
+        this.menu = menu;
+    }
+
+    public Menu getMenu() {
+        return menu;
+    }
+
+    public void setMenu(Menu menu) {
+        this.menu = menu;
+    }
+
+    public Iterable<Cheese> getCheeses() {
+        return cheeses;
+    }
+
+    public void setCheeses(Iterable<Cheese> cheeses) {
+        this.cheeses = cheeses;
+    }
+
+    public int getMenuId() {
+        return menuId;
+    }
+
+    public void setMenuId(int menuId) {
+        this.menuId = menuId;
+    }
+
+    public int getCheeseId() {
+        return cheeseId;
+    }
+
+    public void setCheeseId(int cheeseId) {
+        this.cheeseId = cheeseId;
+    }
+}
