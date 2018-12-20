ac4adee97dea799aed62c82cf3e88569dfa022e4
setite <setyte@gmail.com>
2018-12-19 20:03:33 -0600

    almost done, switching computers

--- src/main/java/org/launchcode/models/Cheese.java
+++ src/main/java/org/launchcode/models/Cheese.java
@@ -1,10 +1,13 @@
 package org.launchcode.models;
 
+import javax.persistence.*;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import java.util.List;
 
 /**
  * Created by LaunchCode
@@ -17,14 +20,18 @@ public class Cheese {
     private int id;
 
     @NotNull
-    @Size(min=3, max=15)
+    @Size(min=3, max=15, message="Name must be between 3 and 15 characters!")
     private String name;
 
     @NotNull
     @Size(min=1, message = "Description must not be empty")
     private String description;
 
-    private CheeseType type;
+    @ManyToOne
+    private Category category;
+
+    @ManyToMany(mappedBy = "cheeses")
+    private List<Menu> menus;
 
     public Cheese(String name, String description) {
         this.name = name;
@@ -33,13 +40,9 @@ public class Cheese {
 
     public Cheese() { }
 
-    public int getId() {
-        return id;
-    }
+    public int getId() { return id; }
 
-    public String getName() {
-        return name;
-    }
+    public String getName() { return name; }
 
     public void setName(String name) {
         this.name = name;
@@ -53,11 +56,7 @@ public class Cheese {
         this.description = description;
     }
 
-    public CheeseType getType() {
-        return type;
-    }
+    public Category getCategory() { return category; }
 
-    public void setType(CheeseType type) {
-        this.type = type;
-    }
+    public void setCategory(Category category) { this.category = category; }
 }
---
+++ src/main/java/org/launchcode/models/Menu.java
@@ -0,0 +1,61 @@
+package org.launchcode.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.*;
+
+@Entity
+public class Menu {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @NotNull
+    @Size(min=3, max=15, message="Name must be between 3 and 15 characters!")
+    private String name;
+
+    @ManyToMany
+    private List<Cheese> cheeses;
+
+    public Menu() {    }
+
+    public Menu(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Cheese> getCheeses() {
+        return cheeses;
+    }
+
+    public void addItem(Cheese item) {
+        cheeses.add(item);
+    }
+
+    public void removeItem(Cheese item) {
+        cheeses.remove(item);
+    }
+
+
+
+}
